df7c77a4ccd5ace8ba58972f6c9787f0200bf46b
sakawat-sakib <mdsakawatsakib@gmail.com>
2024-12-10 07:20:11 +0600

    nw

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
